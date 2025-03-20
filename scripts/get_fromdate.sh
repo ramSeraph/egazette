663e38e82f04bb2d51256f1e99e1e85d771f85ba
ramSeraph <kandimalla.sreeram@gmail.com>
2025-03-20 22:26:01 +0530

    better way of doing fromdate

--- scripts/get_fromdate.sh
+++ scripts/get_fromdate.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+convert_to_short_date() {
+  if [[ "$(uname)" == "Darwin" ]]; then
+    date -j -f "%Y-%m-%dT%H:%M:%SZ" "$1" +%d-%m-%Y
+  else
+    date -d "$1" +%d-%m-%Y
+  fi
+}
+
 usage="USAGE: $0 <source> [<output_file>]"
 src_name=$1
 [[ $src_name == '' ]] && echo "ERROR: missing <source> argument" && echo $usage && exit 1
@@ -12,7 +20,7 @@ echo "got source: $src_name"
 
 echo "getting identifier prefix"
 to_sandox_val='False'
-if [[ $TESTING == '1' ]]; then
+if [[ ${TESTING-} == '1' ]]; then
   to_sandox_val='True'
 fi
 prefix=$(uv run python -c "from srcs.datasrcs_info import get_prefix; print(get_prefix('$src_name', to_sandbox=$to_sandox_val))")
@@ -20,17 +28,18 @@ echo "got prefix as: $prefix"
 [[ $prefix != '' ]] || exit 1
 
 echo "getting items with prefix from internet archive"
-last_id=$(uvx --from internetarchive ia search $prefix --sort 'date asc' -i | tail -1)
-echo "got last id as: $last_id"
+from_date_ia=$(uvx --from internetarchive ia search $prefix --sort 'date desc' -f date --parameters="page=1&rows=1" | jq -r .date)
+if [[ $from_date_ia != '' ]]; then
+  from_date=$(convert_to_short_date $from_date_ia)
+  echo "got from date as : $from_date"
+fi
 
-if [[ $last_id == '' ]]; then
+if [[ ${from_date-} == '' ]]; then
   echo "getting from date from config"
   from_date=$(uv run python -c "from srcs.datasrcs_info import get_start_date; d = get_start_date('$src_name'); print('' if d is None else d.strftime('%d-%m-%Y'))")
-else
-  echo "getting from date from the last id"
-  from_date=$(uvx --from internetarchive ia metadata $last_id | jq -r .metadata.date | awk -v FS=- -v OFS=- '{print $3,$2,$1}')
+  echo "got from date as: $from_date"
 fi
-echo "got from date as: $from_date"
+
 [[ $from_date != '' ]] || exit 1
 
 [[ $output_file == '' ]] && exit 0
