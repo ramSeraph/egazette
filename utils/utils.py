3cf65a857f9234b1095431c7ee4d6328c83948d9
ramSeraph <kandimalla.sreeram@gmail.com>
2025-03-27 00:59:52 +0530

    guess a few more expected filetypes

--- utils/utils.py
+++ utils/utils.py
@@ -334,6 +334,12 @@ def get_file_extension(mtype):
         return 'txt'
     elif re.match('image/png', mtype):
         return 'png'
+    elif re.match('application/msword', mtype):
+        return 'doc'
+    elif re.match('text/rtf', mtype):
+        return 'rtf'
+    elif re.match('application/vnd.ms-excel', mtype):
+        return 'xls'
     return 'unkwn'
 
 def extract_links_from_pdf(fileobj):
