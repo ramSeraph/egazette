9c37e7f6a827134922ed6ba9406ddbca79b7f8e0
ramSeraph <kandimalla.sreeram@gmail.com>
2025-03-26 22:46:20 +0530

    add function to convert text pdf into img pdf, use mupdf, move extract links to mupdf as well

---
+++ utils/pdf_ops.py
@@ -0,0 +1,43 @@
+import pymupdf
+
+def extract_links_from_pdf(fileobj):
+    all_links = []
+
+    doc = pymupdf.open(fileobj)
+    for page in doc:
+        links = [ link.get('file', None) for link in page.get_links() ]
+        all_links.extend([ link for link in links if link is not None ])
+
+    return all_links
+
+def convert_to_image_pdf(file_bytes):
+    outdoc = pymupdf.open()
+
+    doc = pymupdf.open(stream = file_bytes, filetype = 'pdf')
+    for page in doc:
+
+        img_bytes = page.get_pixmap(alpha=False, dpi=300)\
+                        .tobytes(output='jpg', jpg_quality=10)
+
+        img           = pymupdf.open(stream=img_bytes, filetype='jpg')
+        img_pdf_bytes = img.convert_to_pdf()
+        img_rect      = img[0].rect
+
+        img.close()
+
+        img_pdf = pymupdf.open(stream = img_pdf_bytes)
+
+        page = outdoc.new_page(width  = img_rect.width,
+                               height = img_rect.height)
+
+        page.show_pdf_page(img_rect, img_pdf, 0)
+
+    return outdoc.tobytes()
+
+
+if __name__ == '__main__':
+    import sys
+    from pathlib import Path
+    pdf_bytes = convert_to_image_pdf(Path(sys.argv[1]).read_bytes())
+    Path(sys.argv[2]).write_bytes(pdf_bytes)
+
